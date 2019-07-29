663b7a872028b795342c3a33656dc43e7e4a5f51
wangmi <wangmi@diligrp.com>
2019-07-29 12:23:55 +0800

    3.2.9 更新版本号

--- ss-mvc/src/main/java/com/dili/ss/mvc/boot/Swagger2.java
+++ ss-mvc/src/main/java/com/dili/ss/mvc/boot/Swagger2.java
@@ -1,17 +1,21 @@
 package com.dili.ss.mvc.boot;
 
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 /**
  * 访问地址:http://localhost/swagger-ui.html
@@ -25,14 +29,39 @@ public class Swagger2 {
     @Value("${swagger.basePackage}")
     private String basePackage;
 
+    // 定义分隔符
+    private static final String SPLITOR = ",";
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage(basePackage))
+//                .apis(RequestHandlerSelectors.basePackage(basePackage))
+                .apis(basePackage(basePackage))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    public static Predicate<RequestHandler> basePackage(final String basePackage) {
+        return input -> declaringClass(input).transform(handlerPackage(basePackage)).or(true);
+    }
+
+    private static Function<Class<?>, Boolean> handlerPackage(final String basePackage)     {
+        return input -> {
+            // 循环判断匹配
+            for (String strPackage : basePackage.split(SPLITOR)) {
+                boolean isMatch = input.getPackage().getName().startsWith(strPackage);
+                if (isMatch) {
+                    return true;
+                }
+            }
+            return false;
+        };
+    }
+
+    private static Optional<? extends Class<?>> declaringClass(RequestHandler input) {
+        return Optional.fromNullable(input.declaringClass());
     }
 
     private ApiInfo apiInfo() {
