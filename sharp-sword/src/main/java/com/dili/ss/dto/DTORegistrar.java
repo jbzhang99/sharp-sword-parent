2689db53632a36ba4641dd00d8f420abec24fd1c
wangmi <wangmi@diligrp.com>
2019-08-15 10:34:06 +0800

    3.3.0 master DTORegistrar去掉未使用的方法

--- sharp-sword/src/main/java/com/dili/ss/dto/DTORegistrar.java
+++ sharp-sword/src/main/java/com/dili/ss/dto/DTORegistrar.java
@@ -6,12 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.type.AnnotationMetadata;
-import org.springframework.util.ClassUtils;
-import org.springframework.util.StringUtils;
-
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * DTO注册
@@ -28,25 +22,6 @@ public class DTORegistrar implements ImportBeanDefinitionRegistrar {
             ((Class)B.b.g("DTOFactory")).getMethod("registerDTOInstance", AnnotationMetadata.class).invoke(null, annotationMetadata);
         } catch (Exception e) {
         }
-    }
-
-    protected Set<String> getBasePackages(AnnotationMetadata importingClassMetadata) {
-        Map attributes = importingClassMetadata.getAnnotationAttributes(DTOScan.class.getCanonicalName());
-        HashSet basePackages = new HashSet();
-        String[] values = (String[]) attributes.get("value");
-        int valuesLength = values.length;
-        int index;
-        String pkg;
-        for (index = 0; index < valuesLength; ++index) {
-            pkg = values[index];
-            if (StringUtils.hasText(pkg)) {
-                basePackages.add(pkg);
-            }
-        }
-        if (basePackages.isEmpty()) {
-            basePackages.add(ClassUtils.getPackageName(importingClassMetadata.getClassName()));
-        }
-        return basePackages;
     }
 
 }
