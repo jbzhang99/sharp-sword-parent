88744abd62bd3f46d82484101fe2c3e5f9446c2b
wangmi <wangmi@diligrp.com>
2019-09-09 08:59:49 +0800

    3.3.0 master 修改DateUtils获取当前时间，时区为ZoneId.of("GMT+08:00")

--- ss-core/src/main/java/com/dili/ss/util/DateUtils.java
+++ ss-core/src/main/java/com/dili/ss/util/DateUtils.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
@@ -32,12 +33,12 @@ public class DateUtils {
     }
 
     /**
-     * 当前日期格式化
+     * 当前日期格式化(时区为GMT+08:00)
      * @param format
      * @return
      */
     public static String format(String format) {
-        return format(LocalDateTime.now(), format);
+        return format(LocalDateTime.now(ZoneId.of("GMT+08:00")), format);
     }
 
     /**
