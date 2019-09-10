c0a3dcf3e91d547b18e02a5080939dbaea2e6602
wangmi <wangmi@diligrp.com>
2019-09-10 15:03:39 +0800

    3.3.0 src ss-uid优化代码

--- ss-uid/src/main/java/com/dili/ss/uid/handler/BizNumberManager.java
+++ ss-uid/src/main/java/com/dili/ss/uid/handler/BizNumberManager.java
@@ -1,9 +1,6 @@
 package com.dili.ss.uid.handler;
 
 public interface BizNumberManager {
-    void setFixedStep(int fixedStep);
-
-    void setRangeStep(int rangeStep);
 
     void setBizNumberComponent(BizNumberComponent bizNumberComponent);
 
@@ -12,9 +9,20 @@ public interface BizNumberManager {
      * @param type
      * @param dateFormat
      * @param length
-     * @param range
+     * @param step
+     * @param increment
      * @return
      */
-    Long getBizNumberByType(String type, String dateFormat, int length, String range);
+    Long getBizNumberByType(String type, String dateFormat, int length, long step, int increment);
+
+    /**
+     * 根据业务类型获取增量为1的业务号
+     * @param type
+     * @param dateFormat
+     * @param length
+     * @param step
+     * @return
+     */
+    Long getBizNumberByType(String type, String dateFormat, int length, long step);
 
 }
